55956af10263806fecdba9032c5c61a00009ec80
m0bileDev <damian.ogorek.1996@gmail.com>
2024-07-05 18:55:25 +0200

    Function updated to be more reusable (not tested yet);

--- app/src/main/java/com/example/samsungclockclone/ui/customViews/SwipeableClock.kt
+++ app/src/main/java/com/example/samsungclockclone/ui/customViews/SwipeableClock.kt
@@ -3,7 +3,6 @@
 package com.example.samsungclockclone.ui.customViews
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,37 +17,53 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.samsungclockclone.data.utils.TimeFormat
 import com.example.samsungclockclone.data.utils.convertTimeFormatToString
 import com.example.samsungclockclone.data.utils.formatTimeValue
 import com.example.samsungclockclone.ui.theme.SamsungClockCloneTheme
 
-const val PAGER_COUNT = 1000
-const val START_PAGE = PAGER_COUNT / 2
-
+/**
+ * Swipable clock widget. Parameters value are created to show at once max
+ * three elements in column.
+ */
 @Composable
-fun SwipeableClock(modifier: Modifier, timeFormat: TimeFormat, onValueChanged: (Int) -> Unit) {
+fun SwipeableClock(
+    modifier: Modifier,
+    timeFormat: TimeFormat,
+    clockCount: Int = 1000,
+    clockStartPoint: Int = clockCount / 2,
+    swipeableAreaHeight: Dp = 300.dp,
+    swipeableAreaWidth: Dp = 150.dp,
+    itemsPadding: Dp = swipeableAreaHeight / 3,
+    itemSize: Dp = swipeableAreaHeight / 3,
+    itemsFlungAtOnce: Int = 12,
+    fontSize: TextUnit = 40.sp,
+    onValueChanged: (Int) -> Unit
+) {
 
     val pagerState = rememberPagerState(pageCount = {
-        PAGER_COUNT
+        clockCount
     })
     val fling = PagerDefaults.flingBehavior(
         state = pagerState,
-        pagerSnapDistance = PagerSnapDistance.atMost(12)
+        pagerSnapDistance = PagerSnapDistance.atMost(itemsFlungAtOnce)
     )
 
     LaunchedEffect(key1 = pagerState) {
-        pagerState.scrollToPage(START_PAGE)
+        pagerState.scrollToPage(clockStartPoint)
     }
 
     LaunchedEffect(key1 = pagerState) {
         snapshotFlow { pagerState.currentPage }.collect {
             val formattedTimeValue =
-                formatTimeValue(it % timeFormat.format - (START_PAGE), timeFormat)
+                formatTimeValue(it % timeFormat.format - (clockStartPoint), timeFormat)
             onValueChanged(formattedTimeValue)
         }
     }
@@ -56,18 +71,24 @@ fun SwipeableClock(modifier: Modifier, timeFormat: TimeFormat, onValueChanged: (
     Box(modifier = modifier) {
         VerticalPager(
             modifier = Modifier
-                .height(200.dp)
-                .width(150.dp),
+                .height(swipeableAreaHeight)
+                .width(swipeableAreaWidth),
             state = pagerState,
             flingBehavior = fling,
-            contentPadding = PaddingValues(vertical = 70.dp)
+            contentPadding = PaddingValues(vertical = itemsPadding),
         ) {
-            Text(
-                modifier = Modifier
-                    .size(50.dp)
-                    .background(Color.Green),
-                text = convertTimeFormatToString(it % timeFormat.format - (START_PAGE), timeFormat)
-            )
+            Box(
+                modifier = Modifier.size(itemSize),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    fontSize = fontSize,
+                    text = convertTimeFormatToString(
+                        it % timeFormat.format - (clockStartPoint),
+                        timeFormat
+                    )
+                )
+            }
         }
     }
 }
