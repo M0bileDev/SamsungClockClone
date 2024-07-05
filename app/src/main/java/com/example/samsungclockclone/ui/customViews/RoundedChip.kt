5a86a23a71b3ebe18d6fb4ca0248e40f868d0f9c
m0bileDev <damian.ogorek.1996@gmail.com>
2024-07-05 23:25:04 +0200

    Widget updated (fixed text positioning );

--- app/src/main/java/com/example/samsungclockclone/ui/customViews/RoundedChip.kt
+++ app/src/main/java/com/example/samsungclockclone/ui/customViews/RoundedChip.kt
@@ -1,6 +1,7 @@
 package com.example.samsungclockclone.ui.customViews
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.FilterChip
@@ -10,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -55,10 +57,13 @@ fun <T> RoundedChip(
         border = border,
         selected = selected,
         label = {
-            Text(
-                text = value.toString(),
-                color = textColor
-            )
+            Box {
+                Text(
+                    textAlign = TextAlign.Center,
+                    text = value.toString(),
+                    color = textColor
+                )
+            }
         },
         onClick = {
             onSelected(value)
