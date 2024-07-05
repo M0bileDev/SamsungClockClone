25c6160a778cff9a375511f470ca3868b54709da
m0bileDev <damian.ogorek.1996@gmail.com>
2024-07-05 23:17:42 +0200

    Ui state skeleton added;

--- app/src/main/java/com/example/samsungclockclone/presentation/addAlarm/AddAlarmViewModel.kt
+++ app/src/main/java/com/example/samsungclockclone/presentation/addAlarm/AddAlarmViewModel.kt
@@ -1,5 +1,12 @@
 package com.example.samsungclockclone.presentation.addAlarm
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
-class AddAlarmViewModel : ViewModel()
+class AddAlarmViewModel : ViewModel() {
+
+    private val addAlarmUiState = MutableStateFlow(AddAlarmUiState())
+    val uiState = addAlarmUiState.asStateFlow()
+
+}
