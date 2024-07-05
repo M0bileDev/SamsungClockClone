c832d10a60c3c792dca53a22c6fa0878b7a976b0
m0bileDev <damian.ogorek.1996@gmail.com>
2024-07-05 23:17:48 +0200

    Ui state skeleton added;

--- app/src/main/java/com/example/samsungclockclone/presentation/addAlarm/AddAlarmUiState.kt
+++ app/src/main/java/com/example/samsungclockclone/presentation/addAlarm/AddAlarmUiState.kt
@@ -1,5 +1,6 @@
 package com.example.samsungclockclone.presentation.addAlarm
 
 data class AddAlarmUiState(
-    val nothing: Nothing
+    val daysOfWeek: List<String> = emptyList(),
+    val selectedDaysOfWeek: List<String> = emptyList()
 )
