b55d6aacb8615096613ab1b6e72fa9635f06b442
m0bileDev <damian.ogorek.1996@gmail.com>
2024-07-08 22:33:15 +0200

    Preview ui state created;

--- app/src/main/java/com/example/samsungclockclone/presentation/addAlarm/AddAlarmUiState.kt
+++ app/src/main/java/com/example/samsungclockclone/presentation/addAlarm/AddAlarmUiState.kt
@@ -4,16 +4,36 @@ import com.example.samsungclockclone.presentation.addAlarm.model.DayOfWeek
 import com.example.samsungclockclone.presentation.addAlarm.model.DayOfWeek.DayOfWeekHelper.standardWeek
 
 data class AddAlarmUiState(
+    val alarmScheduleInfo: String = "",
     //May change, depend of the settings configuration
     val daysOfWeek: List<DayOfWeek> = standardWeek(),
-
     val selectedDaysOfWeek: List<DayOfWeek> = emptyList(),
     val alarmName: String = "",
-    val soundName: String = "Default",
-    val soundEnabled: Boolean = true,
-    val vibrationName: String = "Default",
-    val vibrationEnabled: Boolean = true,
-    val snoozeIntervalName: String = "5 minutes",
-    val snoozeRepeatName: String = "3 times",
-    val snoozeEnabled: Boolean = true
-)
+    val soundName: String = "",
+    val soundEnabled: Boolean = false,
+    val vibrationName: String = "",
+    val vibrationEnabled: Boolean = false,
+    val snoozeIntervalName: String = "",
+    val snoozeRepeatName: String = "",
+    val snoozeEnabled: Boolean = false
+) {
+    companion object AddAlarmUiStateHelper {
+        val alarmUiStatePreview = AddAlarmUiState(
+            alarmScheduleInfo = "Tomorrow-Sat, 6 Jul",
+            daysOfWeek = DayOfWeek.DayOfWeekHelper.standardWeek(),
+            selectedDaysOfWeek = listOf(
+                DayOfWeek.Monday(),
+                DayOfWeek.Saturday(),
+                DayOfWeek.Sunday()
+            ),
+            alarmName = "Alarm 1",
+            soundName = "Default",
+            soundEnabled = true,
+            vibrationName = "Default",
+            vibrationEnabled = true,
+            snoozeIntervalName = "5 minutes",
+            snoozeRepeatName = "3 times",
+            snoozeEnabled = true
+        )
+    }
+}
