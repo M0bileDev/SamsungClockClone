31bfc6cff4e3f9b52d264b3db403cdd2effb4a04
m0bileDev <damian.ogorek.1996@gmail.com>
2024-07-08 22:18:28 +0200

    Ui state updated; Concrete types created;

--- app/src/main/java/com/example/samsungclockclone/presentation/addAlarm/AddAlarmUiState.kt
+++ app/src/main/java/com/example/samsungclockclone/presentation/addAlarm/AddAlarmUiState.kt
@@ -1,6 +1,19 @@
 package com.example.samsungclockclone.presentation.addAlarm
 
+import com.example.samsungclockclone.presentation.addAlarm.model.DayOfWeek
+import com.example.samsungclockclone.presentation.addAlarm.model.DayOfWeek.DayOfWeekHelper.standardWeek
+
 data class AddAlarmUiState(
-    val daysOfWeek: List<String> = emptyList(),
-    val selectedDaysOfWeek: List<String> = emptyList()
+    //May change, depend of the settings configuration
+    val daysOfWeek: List<DayOfWeek> = standardWeek(),
+
+    val selectedDaysOfWeek: List<DayOfWeek> = emptyList(),
+    val alarmName: String = "",
+    val soundName: String = "Default",
+    val soundEnabled: Boolean = true,
+    val vibrationName: String = "Default",
+    val vibrationEnabled: Boolean = true,
+    val snoozeIntervalName: String = "5 minutes",
+    val snoozeRepeatName: String = "3 times",
+    val snoozeEnabled: Boolean = true
 )
