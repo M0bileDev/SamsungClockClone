2309d176009dcb5fe08d894dc81f6a161d47d9c3
m0bileDev <damian.ogorek.1996@gmail.com>
2024-07-11 22:00:05 +0200

    Hilt application created;

---
+++ app/src/main/java/com/example/samsungclockclone/application/SamsungClockCloneApplication.kt
@@ -0,0 +1,7 @@
+package com.example.samsungclockclone.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SamsungClockCloneApplication : Application()
