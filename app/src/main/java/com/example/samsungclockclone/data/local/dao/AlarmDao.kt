d69a8c22979572ddb36c1049dd1de8a28a15615a
m0bileDev <damian.ogorek.1996@gmail.com>
2024-07-12 21:49:08 +0200

    Dao created;

---
+++ app/src/main/java/com/example/samsungclockclone/data/local/dao/AlarmDao.kt
@@ -0,0 +1,42 @@
+package com.example.samsungclockclone.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.samsungclockclone.data.local.model.AlarmEntity
+import com.example.samsungclockclone.data.local.model.AlarmManagerEntity
+import com.example.samsungclockclone.data.local.model.AlarmWithAlarmManagerEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface AlarmDao {
+
+    @Query("SELECT * FROM alarm_table WHERE id =:alarmId")
+    fun getAlarmAndAlarmManagers(
+        alarmId: Long
+    ): Flow<List<AlarmWithAlarmManagerEntity>>
+
+    @Query("SELECT * FROM alarm_table")
+    fun getAllAlarms(): Flow<List<AlarmEntity>>
+
+    @Query("SELECT * FROM alarm_manager_table ORDER BY fireTime ASC LIMIT 1")
+    fun getNearestAlarmManager(): Flow<AlarmManagerEntity>
+
+    @Insert
+    suspend fun insertAlarm(alarm: AlarmEntity)
+
+    @Update
+    suspend fun updateAlarm(alarm: AlarmEntity)
+
+    @Delete
+    suspend fun deleteAlarm(alarmEntity: AlarmEntity)
+
+    @Insert
+    suspend fun insertAlarmManager(alarmManagerEntity: AlarmManagerEntity)
+
+    @Delete
+    suspend fun deleteAlarmManager(alarmManagerEntity: AlarmManagerEntity)
+
+}
