7cc2e26dea668a216e30531f1c532eed07e87668
m0bileDev <damian.ogorek.1996@gmail.com>
2024-07-12 21:48:35 +0200

    Application scoped module created; Alarm manager provider created; Database provider created;

---
+++ app/src/main/java/com/example/samsungclockclone/di/ApplicationModule.kt
@@ -0,0 +1,43 @@
+package com.example.samsungclockclone.di
+
+import android.app.AlarmManager
+import android.content.Context
+import androidx.room.Room
+import com.example.samsungclockclone.data.local.dao.AlarmDao
+import com.example.samsungclockclone.data.local.db.AlarmDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@[Module InstallIn(SingletonComponent::class)]
+object ApplicationModule {
+
+    @[Provides Singleton]
+    fun provideAlarmManager(
+        @ApplicationContext context: Context
+    ): AlarmManager {
+        return context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+    }
+
+    @[Provides Singleton]
+    fun provideAlarmDatabase(
+        @ApplicationContext context: Context
+    ): AlarmDatabase {
+        return Room.databaseBuilder(
+            context,
+            AlarmDatabase::class.java,
+            "alarm-database"
+        ).build()
+    }
+
+    @[Provides Singleton]
+    fun provideAlarmDao(
+        alarmDatabase: AlarmDatabase
+    ): AlarmDao {
+        return alarmDatabase.alarmDao()
+    }
+
+}
