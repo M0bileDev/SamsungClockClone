4105fee3dd68c5ca56b6fcdd2bf87d5d0afc43bd
m0bileDev <damian.ogorek.1996@gmail.com>
2024-07-13 19:41:32 +0200

    Viewmodel logic updated; Arguments updated;

--- app/src/main/java/com/example/samsungclockclone/presentation/addAlarm/AddAlarmViewModel.kt
+++ app/src/main/java/com/example/samsungclockclone/presentation/addAlarm/AddAlarmViewModel.kt
@@ -1,8 +1,13 @@
 package com.example.samsungclockclone.presentation.addAlarm
 
-import android.app.AlarmManager
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.samsungclockclone.data.local.dao.AlarmDao
+import com.example.samsungclockclone.data.local.model.AlarmEntity
+import com.example.samsungclockclone.data.local.model.AlarmManagerEntity
+import com.example.samsungclockclone.data.local.scheduler.AlarmScheduler
 import com.example.samsungclockclone.presentation.addAlarm.model.AlarmMode
+import com.example.samsungclockclone.presentation.addAlarm.model.AlarmRepeat
 import com.example.samsungclockclone.presentation.addAlarm.model.DayOfWeek
 import com.example.samsungclockclone.presentation.addAlarm.model.DayOfWeek.DayOfWeekHelper.convertCalendarDayOfWeekToDayOfWeek
 import com.example.samsungclockclone.presentation.addAlarm.model.DayOfWeek.DayOfWeekHelper.differenceBetweenDays
@@ -10,6 +15,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import java.time.LocalDateTime
 import java.time.ZoneId
 import java.util.Calendar
@@ -17,14 +23,13 @@ import javax.inject.Inject
 
 @HiltViewModel
 class AddAlarmViewModel @Inject constructor(
-    private val alarmManager: AlarmManager
+    private val alarmScheduler: AlarmScheduler,
+    private val alarmDao: AlarmDao
 ) : ViewModel() {
 
     private val addAlarmUiState = MutableStateFlow(AddAlarmUiState())
     val uiState = addAlarmUiState.asStateFlow()
 
-    private var actualDateTime = LocalDateTime.now()
-    private var alarmMilliseconds = 0L
     private var alarmHour = 0
     private var alarmMinute = 0
     private var alarmMode = AlarmMode.OnlyTime
@@ -81,14 +86,38 @@ class AddAlarmViewModel @Inject constructor(
         }
     }
 
+    private fun createAlarmRepeat(): AlarmRepeat {
+        return when (alarmMode) {
+            AlarmMode.OnlyTime -> AlarmRepeat.EveryDay
+            AlarmMode.DayOfWeekAndTime -> AlarmRepeat.EveryWeek
+            AlarmMode.CalendarDateAndTime -> AlarmRepeat.EveryDay
+        }
+    }
+
+
     fun onSave() {
         val alarmMillisecondsList = createAlarmMilliseconds()
 
+        //save alarm information inside database
+        viewModelScope.launch {
+            val alarmName = addAlarmUiState.value.alarmName
+            val alarmEntity = AlarmEntity(name = alarmName, enable = true)
+            val alarmId = alarmDao.insertAlarm(alarmEntity)
 
+            val alarmRepeat = createAlarmRepeat()
+            val alarms = alarmMillisecondsList.map { alarmMilliseconds ->
+                val alarmManagerEntity = AlarmManagerEntity(
+                    parentId = alarmId,
+                    fireTime = alarmMilliseconds,
+                    repeat = alarmRepeat.name
+                )
+                alarmMilliseconds to alarmDao.insertAlarmManager(alarmManagerEntity)
+            }
 
-        alarmMillisecondsList.forEach { alarmMilliseconds ->
-            //create alarms with alarm manager
-            //save alarm information inside database
+            //save alarms via AlarmManager
+            alarms.forEach { alarm ->
+                alarmScheduler.schedule(alarm.first, alarm.second)
+            }
         }
 
     }
@@ -153,8 +182,6 @@ class AddAlarmViewModel @Inject constructor(
             }
         }
     }
-
-
 }
 
 
