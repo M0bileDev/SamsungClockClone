aa581c66d7a8a8f5d9b3748459d31800250d7315
m0bileDev <damian.ogorek.1996@gmail.com>
2024-07-13 19:37:14 +0200

    Broadcast receiver created to handle alarm invocation;

---
+++ app/src/main/java/com/example/samsungclockclone/data/local/receiver/AlarmReceiver.kt
@@ -0,0 +1,14 @@
+package com.example.samsungclockclone.data.local.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+const val ALARM_ID_KEY = "ALARM_ID"
+
+class AlarmReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("Not yet implemented")
+    }
+}
