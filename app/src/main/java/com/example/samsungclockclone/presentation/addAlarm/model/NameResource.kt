36441a48c3d65b3bd92a5b6162a4cee8bc48959c
m0bileDev <damian.ogorek.1996@gmail.com>
2024-07-17 21:53:31 +0200

    Type name updated;

--- app/src/main/java/com/example/samsungclockclone/presentation/addAlarm/model/NameResource.kt
+++ app/src/main/java/com/example/samsungclockclone/presentation/addAlarm/model/NameResource.kt
@@ -2,7 +2,7 @@ package com.example.samsungclockclone.presentation.addAlarm.model
 
 import androidx.annotation.StringRes
 
-interface ShortName {
+interface NameResource {
     @get:StringRes
     val nameResourceValue: Int
 }
