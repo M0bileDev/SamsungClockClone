3275515bcb588982a0591f3d4ddad9fc57871739
m0bileDev <damian.ogorek.1996@gmail.com>
2024-07-18 21:49:00 +0200

    Imports updated;

--- app/src/main/java/com/example/samsungclockclone/ui/customViews/RoundedChip.kt
+++ app/src/main/java/com/example/samsungclockclone/ui/customViews/RoundedChip.kt
@@ -20,7 +20,7 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.samsungclockclone.presentation.addAlarm.model.NameResource
+import com.example.samsungclockclone.domain.model.NameResource
 import com.example.samsungclockclone.ui.theme.SamsungClockCloneTheme
 
 @Composable
