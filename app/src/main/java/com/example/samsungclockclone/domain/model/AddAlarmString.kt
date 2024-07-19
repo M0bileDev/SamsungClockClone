b01f43c9be75f5c3b8d88c0b08fdcae565523883
m0bileDev <damian.ogorek.1996@gmail.com>
2024-07-19 19:04:42 +0200

    Imports updated;

--- app/src/main/java/com/example/samsungclockclone/domain/model/AddAlarmString.kt
+++ app/src/main/java/com/example/samsungclockclone/domain/model/AddAlarmString.kt
@@ -1,6 +1,9 @@
 package com.example.samsungclockclone.domain.model
 
-data class AddAlarmString (
+import com.example.samsungclockclone.domain.utils.DayOfWeek
+import com.example.samsungclockclone.presentation.addAlarm.utils.AddAlarmStringType
+
+data class AddAlarmString(
     val type: AddAlarmStringType = AddAlarmStringType.NotDefined,
     val stringValue: String = "",
     val daysOfWeek: List<DayOfWeek> = emptyList()
