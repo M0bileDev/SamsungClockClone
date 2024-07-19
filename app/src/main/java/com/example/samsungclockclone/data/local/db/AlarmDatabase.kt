8822237446fab053e65f8a64ff9a14730dc3af3b
m0bileDev <damian.ogorek.1996@gmail.com>
2024-07-19 19:04:05 +0200

    Database updated; Type converters added;

--- app/src/main/java/com/example/samsungclockclone/data/local/db/AlarmDatabase.kt
+++ app/src/main/java/com/example/samsungclockclone/data/local/db/AlarmDatabase.kt
@@ -2,13 +2,23 @@ package com.example.samsungclockclone.data.local.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.samsungclockclone.data.local.converter.AlarmModeConverter
+import com.example.samsungclockclone.data.local.converter.AlarmRepeatConverter
 import com.example.samsungclockclone.data.local.dao.AlarmDao
 import com.example.samsungclockclone.data.local.model.AlarmEntity
 import com.example.samsungclockclone.data.local.model.AlarmManagerEntity
 
 @Database(
-    entities = [AlarmEntity::class, AlarmManagerEntity::class],
+    entities = [
+        AlarmEntity::class,
+        AlarmManagerEntity::class
+    ],
     version = 1
+)
+@TypeConverters(
+    AlarmModeConverter::class,
+    AlarmRepeatConverter::class
 )
 abstract class AlarmDatabase : RoomDatabase() {
     abstract fun alarmDao(): AlarmDao
