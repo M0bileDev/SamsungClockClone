fbf5adf5451e92153a4c1f27ce3964a65a47409d
m0bileDev <damian.ogorek.1996@gmail.com>
2024-07-19 19:03:36 +0200

    Entity type updated;

--- app/src/main/java/com/example/samsungclockclone/data/local/model/AlarmEntity.kt
+++ app/src/main/java/com/example/samsungclockclone/data/local/model/AlarmEntity.kt
@@ -2,11 +2,13 @@ package com.example.samsungclockclone.data.local.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.samsungclockclone.domain.utils.AlarmMode
 
 @Entity(tableName = "alarm_table")
 data class AlarmEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0L,
+    val mode: AlarmMode,
     val name: String,
     val enable: Boolean
 )
