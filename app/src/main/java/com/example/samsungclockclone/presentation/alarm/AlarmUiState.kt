9d4012f6aa5682888831cf697ff83f9c149d0dbf
m0bileDev <damian.ogorek.1996@gmail.com>
2024-07-22 22:58:23 +0200

    Ui model created;

--- app/src/main/java/com/example/samsungclockclone/presentation/alarm/AlarmUiState.kt
+++ app/src/main/java/com/example/samsungclockclone/presentation/alarm/AlarmUiState.kt
@@ -1,5 +1,10 @@
 package com.example.samsungclockclone.presentation.alarm
 
+import com.example.samsungclockclone.domain.model.alarm.AlarmItem
+
 data class AlarmUiState(
-    val nothing: Nothing
+    val alarmItems: List<AlarmItem> = emptyList(),
+    val editAvailable: Boolean = false,
+    val sortAvailable: Boolean = false,
+    val editModeEnable: Boolean = false
 )
