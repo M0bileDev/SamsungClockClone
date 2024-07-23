ebdc55cbe3298f7ea121d63f724f170bd027d211
m0bileDev <damian.ogorek.1996@gmail.com>
2024-07-23 13:56:05 +0200

    Model updated;

--- app/src/main/java/com/example/samsungclockclone/domain/model/alarm/AlarmItem.kt
+++ app/src/main/java/com/example/samsungclockclone/domain/model/alarm/AlarmItem.kt
@@ -1,9 +1,22 @@
 package com.example.samsungclockclone.domain.model.alarm
 
+import com.example.samsungclockclone.data.local.scheduler.AlarmId
 import com.example.samsungclockclone.domain.utils.AlarmMode
 
 data class AlarmItem(
+    val alarmId: AlarmId = 0L,
+    val name: String = "",
     val fireTime: Long = 0L,
     val mode: AlarmMode = AlarmMode.OnlyTime,
     val enable: Boolean = false
-)
+) {
+    companion object {
+        val alarmItemPreview = AlarmItem(
+            0L,
+            "Preview",
+            1721730918345,
+            AlarmMode.OnlyTime,
+            enable = true
+        )
+    }
+}
