bb3d11207183577fcd722a1c799e9e605ad88663
m0bileDev <damian.ogorek.1996@gmail.com>
2024-07-24 12:45:02 +0200

    Fields updated; Previews updated;

--- app/src/main/java/com/example/samsungclockclone/domain/model/alarm/AlarmItem.kt
+++ app/src/main/java/com/example/samsungclockclone/domain/model/alarm/AlarmItem.kt
@@ -2,13 +2,16 @@ package com.example.samsungclockclone.domain.model.alarm
 
 import com.example.samsungclockclone.data.local.scheduler.AlarmId
 import com.example.samsungclockclone.domain.utils.AlarmMode
+import com.example.samsungclockclone.domain.utils.DayOfWeek
 
 data class AlarmItem(
     val alarmId: AlarmId = 0L,
     val name: String = "",
     val fireTime: Long = 0L,
     val mode: AlarmMode = AlarmMode.OnlyTime,
-    val enable: Boolean = false
+    val enable: Boolean = false,
+    val daysOfWeek: List<DayOfWeek> = DayOfWeek.DayOfWeekHelper.standardWeek(),
+    val selectedDaysOfWeek: List<DayOfWeek> = emptyList(),
 ) {
     companion object {
         val alarmItemPreview = AlarmItem(
@@ -17,6 +20,29 @@ data class AlarmItem(
             1721730918345,
             AlarmMode.OnlyTime,
             enable = true
+        )
+        val alarmItemPreview2 = AlarmItem(
+            0L,
+            "Preview",
+            1721730918345,
+            AlarmMode.DayOfWeekAndTime,
+            enable = true,
+            selectedDaysOfWeek = listOf(DayOfWeek.Monday, DayOfWeek.Wednesday, DayOfWeek.Friday)
+        )
+        val alarmItemPreview3 = AlarmItem(
+            0L,
+            "Preview",
+            1721730918345,
+            AlarmMode.DayOfWeekAndTime,
+            enable = true,
+            selectedDaysOfWeek = DayOfWeek.DayOfWeekHelper.standardWeek()
+        )
+        val alarmItemPreview4 = AlarmItem(
+            0L,
+            "",
+            1721730918345,
+            AlarmMode.OnlyTime,
+            enable = false
         )
     }
 }
