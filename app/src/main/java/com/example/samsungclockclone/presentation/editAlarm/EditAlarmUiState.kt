71318cee5605167992b418296f43d498442bce86
m0bileDev <damian.ogorek.1996@gmail.com>
2024-07-26 12:40:22 +0200

    Removed unnecessary fields; Preview ui state created;

--- app/src/main/java/com/example/samsungclockclone/presentation/editAlarm/EditAlarmUiState.kt
+++ app/src/main/java/com/example/samsungclockclone/presentation/editAlarm/EditAlarmUiState.kt
@@ -1,11 +1,31 @@
 package com.example.samsungclockclone.presentation.editAlarm
 
+import com.example.samsungclockclone.domain.model.alarm.AlarmItem
 import com.example.samsungclockclone.domain.model.alarm.EditAlarmItem
 
 data class EditAlarmUiState(
-    val allSelected: Boolean = false,
-    val selectedCount: Int = 0,
     val editAlarmItems: List<EditAlarmItem> = emptyList(),
     // TODO: Create enum type of bottom buttons
-    val bottomButtons
-)
+) {
+    companion object {
+        val editAlarmUiStatePreview = EditAlarmUiState(
+            listOf(
+                EditAlarmItem(
+                    selected = false,
+                    dragged = false,
+                    AlarmItem.alarmItemPreview
+                ),
+                EditAlarmItem(
+                    selected = true,
+                    dragged = false,
+                    AlarmItem.alarmItemPreview2
+                ),
+                EditAlarmItem(
+                    selected = true,
+                    dragged = true,
+                    AlarmItem.alarmItemPreview3
+                ),
+            )
+        )
+    }
+}
