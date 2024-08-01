d4579bc17d4fe006e9a71a97dff9c739da1d85a7
m0bileDev <damian.ogorek.1996@gmail.com>
2024-08-01 14:46:43 +0200

    Ui state fields added; Previews added;

--- app/src/main/java/com/example/samsungclockclone/presentation/editAlarm/EditAlarmUiState.kt
+++ app/src/main/java/com/example/samsungclockclone/presentation/editAlarm/EditAlarmUiState.kt
@@ -5,7 +5,11 @@ import com.example.samsungclockclone.domain.model.alarm.EditAlarmItem
 
 data class EditAlarmUiState(
     val editAlarmItems: List<EditAlarmItem> = emptyList(),
-    val allSelected: Boolean = false
+    val allSelected: Boolean = false,
+    val turnOnEnabled: Boolean = false,
+    val turnOffEnabled: Boolean = false,
+    val deleteEnabled: Boolean = false,
+    val deleteAllEnabled: Boolean = false
     // TODO: Create enum type of bottom buttons
 ) {
     companion object {
@@ -24,6 +28,53 @@ data class EditAlarmUiState(
                 EditAlarmItem(
                     selected = true,
                     dragged = true,
+                    AlarmItem.alarmItemPreview3
+                ),
+            )
+        )
+
+        val editAlarmUiStatePreview2 = EditAlarmUiState(
+            turnOnEnabled = true,
+            turnOffEnabled = true,
+            deleteEnabled = true,
+            editAlarmItems = listOf(
+                EditAlarmItem(
+                    selected = false,
+                    dragged = false,
+                    AlarmItem.alarmItemPreview
+                ),
+                EditAlarmItem(
+                    selected = true,
+                    dragged = false,
+                    AlarmItem.alarmItemPreview2
+                ),
+                EditAlarmItem(
+                    selected = true,
+                    dragged = true,
+                    AlarmItem.alarmItemPreview3
+                ),
+            )
+        )
+
+        val editAlarmUiStatePreview3 = EditAlarmUiState(
+            turnOnEnabled = true,
+            turnOffEnabled = true,
+            deleteAllEnabled = true,
+            allSelected = true,
+            editAlarmItems = listOf(
+                EditAlarmItem(
+                    selected = true,
+                    dragged = false,
+                    AlarmItem.alarmItemPreview
+                ),
+                EditAlarmItem(
+                    selected = true,
+                    dragged = false,
+                    AlarmItem.alarmItemPreview2
+                ),
+                EditAlarmItem(
+                    selected = true,
+                    dragged = false,
                     AlarmItem.alarmItemPreview3
                 ),
             )
