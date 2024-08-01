388b2e842f0e412263c4226b92e40ef4932e716e
m0bileDev <damian.ogorek.1996@gmail.com>
2024-08-01 14:47:51 +0200

    Ui updated;

--- app/src/main/java/com/example/samsungclockclone/presentation/editAlarm/EditAlarmScreen.kt
+++ app/src/main/java/com/example/samsungclockclone/presentation/editAlarm/EditAlarmScreen.kt
@@ -2,37 +2,52 @@
 
 package com.example.samsungclockclone.presentation.editAlarm
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.samsungclockclone.data.local.scheduler.AlarmId
 import com.example.samsungclockclone.presentation.editAlarm.EditAlarmUiState.Companion.editAlarmUiStatePreview
+import com.example.samsungclockclone.presentation.editAlarm.EditAlarmUiState.Companion.editAlarmUiStatePreview2
+import com.example.samsungclockclone.presentation.editAlarm.EditAlarmUiState.Companion.editAlarmUiStatePreview3
 import com.example.samsungclockclone.ui.customViews.EditAlarmItemCard
 import com.example.samsungclockclone.ui.theme.SamsungClockCloneTheme
+import com.example.samsungclockclone.ui.utils.drawables
 
 @Composable
 fun EditAlarmScreen(
     modifier: Modifier = Modifier,
     uiState: EditAlarmUiState,
     onSelectionAllChanged: () -> Unit,
-    onSelectionChanged: (AlarmId) -> Unit
+    onSelectionChanged: (AlarmId) -> Unit,
+    onTurnOn: () -> Unit,
+    onTurnOff: () -> Unit,
+    onDelete: () -> Unit,
+    onDeleteAll: () -> Unit
 ) = with(uiState) {
 
     val topAppBarState = rememberTopAppBarState()
@@ -42,6 +57,14 @@ fun EditAlarmScreen(
         modifier = modifier
             .fillMaxSize()
             .nestedScroll(scrollBehaviour.nestedScrollConnection),
+        bottomBar = {
+            EditAlarmBottomBar(
+                onTurnOn = onTurnOn,
+                onTurnOff = onTurnOff,
+                onDelete = onDelete,
+                onDeleteAll = onDeleteAll
+            )
+        },
         topBar = {
             MediumTopAppBar(
                 title = {
@@ -77,6 +100,72 @@ fun EditAlarmScreen(
     }
 }
 
+@Composable
+private fun EditAlarmUiState.EditAlarmBottomBar(
+    onTurnOn: () -> Unit,
+    onTurnOff: () -> Unit,
+    onDelete: () -> Unit,
+    onDeleteAll: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .defaultMinSize(minHeight = 80.dp)
+            .background(MaterialTheme.colorScheme.surfaceContainer),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        if (turnOnEnabled) {
+            TextButton(onClick = onTurnOn) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Icon(
+                        painter = painterResource(id = drawables.ic_alarm_on),
+                        contentDescription = "Turn on selected alarms"
+                    )
+                    Text("Turn on")
+                }
+            }
+        }
+
+        if (turnOffEnabled) {
+            TextButton(onClick = onTurnOff) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Icon(
+                        painter = painterResource(id = drawables.ic_alarm_off),
+                        contentDescription = "Turn off selected alarms"
+                    )
+                    Text("Turn off")
+                }
+            }
+        }
+
+        if (deleteEnabled) {
+            TextButton(onClick = onDelete) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Icon(
+                        painter = painterResource(id = drawables.ic_delete),
+                        contentDescription = "Delete selected alarms"
+                    )
+                    Text("Delete")
+                }
+            }
+        }
+
+        if (deleteAllEnabled) {
+            TextButton(onClick = onDeleteAll) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Icon(
+                        painter = painterResource(id = drawables.ic_delete),
+                        contentDescription = "Delete all alarms"
+                    )
+                    Text("Delete all")
+                }
+            }
+        }
+
+    }
+}
+
 @Preview
 @Composable
 private fun EditAlarmScreenPreview() {
@@ -84,7 +173,43 @@ private fun EditAlarmScreenPreview() {
         EditAlarmScreen(
             uiState = editAlarmUiStatePreview,
             onSelectionAllChanged = {},
-            onSelectionChanged = {}
+            onSelectionChanged = {},
+            onTurnOn = {},
+            onTurnOff = {},
+            onDelete = {},
+            onDeleteAll = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun EditAlarmScreenPreview2() {
+    SamsungClockCloneTheme {
+        EditAlarmScreen(
+            uiState = editAlarmUiStatePreview2,
+            onSelectionAllChanged = {},
+            onSelectionChanged = {},
+            onTurnOn = {},
+            onTurnOff = {},
+            onDelete = {},
+            onDeleteAll = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun EditAlarmScreenPreview3() {
+    SamsungClockCloneTheme {
+        EditAlarmScreen(
+            uiState = editAlarmUiStatePreview3,
+            onSelectionAllChanged = {},
+            onSelectionChanged = {},
+            onTurnOn = {},
+            onTurnOff = {},
+            onDelete = {},
+            onDeleteAll = {}
         )
     }
 }
