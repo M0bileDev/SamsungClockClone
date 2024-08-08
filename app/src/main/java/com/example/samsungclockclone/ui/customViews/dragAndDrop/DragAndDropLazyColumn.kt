36f5794bbe479d660e191a6a1632c12b1f00fe42
m0bileDev <damian.ogorek.1996@gmail.com>
2024-08-08 15:15:31 +0200

    Logic updated by adding drag condition;Preview updated;

--- app/src/main/java/com/example/samsungclockclone/ui/customViews/dragAndDrop/DragAndDropLazyColumn.kt
+++ app/src/main/java/com/example/samsungclockclone/ui/customViews/dragAndDrop/DragAndDropLazyColumn.kt
@@ -8,34 +8,41 @@ import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerEventType
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.samsungclockclone.ui.customModifier.translationYDragAndDrop
 import com.example.samsungclockclone.ui.customViews.dragAndDrop.ext.move
 import com.example.samsungclockclone.ui.theme.SamsungClockCloneTheme
+import com.example.samsungclockclone.ui.utils.drawables
 import kotlinx.coroutines.launch
 
 @Composable
 fun <T> DragAndDropLazyColumn(
     modifier: Modifier = Modifier,
     items: List<T>,
-    onMove: (Index, Index) -> Unit,
     content: @Composable (T, Boolean) -> Unit,
     contentPadding: PaddingValues = PaddingValues(0.dp),
     reverseLayout: Boolean = false,
@@ -44,10 +51,15 @@ fun <T> DragAndDropLazyColumn(
     horizontalAlignment: Alignment.Horizontal = Alignment.Start,
     flingBehavior: FlingBehavior = ScrollableDefaults.flingBehavior(),
     userScrollEnabled: Boolean = true,
+    onMove: (Index, Index) -> Unit,
+    onDragCondition: () -> Boolean = { true }
 ) {
 
     val coroutineScope = rememberCoroutineScope()
-    val dragAndDropListState = rememberDragAndDropListState(onMove = onMove)
+    val dragAndDropListState = rememberDragAndDropListState(
+        onMove = onMove,
+        onDragCondition = onDragCondition
+    )
 
     LazyColumn(
         modifier = modifier
@@ -100,9 +112,12 @@ fun <T> DragAndDropLazyColumn(
 @Composable
 private fun DragAndDropListStatePreview() {
     SamsungClockCloneTheme {
-
         val editableList = remember {
             mutableListOf("First", "Second", "Third")
+        }
+
+        var iconPress by remember {
+            mutableStateOf(false)
         }
 
         DragAndDropLazyColumn(
@@ -110,14 +125,37 @@ private fun DragAndDropListStatePreview() {
             verticalArrangement = Arrangement.spacedBy(8.dp),
             items = editableList,
             onMove = { fromIndex, toIndex -> editableList.move(fromIndex, toIndex) },
+            onDragCondition = {
+                iconPress
+            },
             content = { text, selected ->
                 Card(
-                    modifier = Modifier
-                        .height(40.dp)
-                        .fillMaxWidth(),
                     border = if (selected) BorderStroke(2.dp, Color.Red) else null
                 ) {
-                    Text(text = text)
+                    Row(
+                        modifier = Modifier
+                            .height(48.dp)
+                            .fillMaxWidth()
+                            .padding(horizontal = 8.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                    ) {
+                        Icon(
+                            modifier = Modifier
+                                .padding(horizontal = 16.dp)
+                                .pointerInput(Unit) {
+                                    awaitPointerEventScope {
+                                        while (true) {
+                                            val event = awaitPointerEvent()
+                                            iconPress = event.type != PointerEventType.Release
+                                        }
+                                    }
+                                },
+                            painter = painterResource(id = drawables.ic_drag),
+                            contentDescription = null
+                        )
+                        Text(text = text)
+                    }
                 }
             }
         )
