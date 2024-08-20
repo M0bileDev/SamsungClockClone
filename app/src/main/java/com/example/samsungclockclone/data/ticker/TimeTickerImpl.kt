d700f7a515db85bf727394c3e04b5af44299d8a1
m0bileDev <damian.ogorek.1996@gmail.com>
2024-08-20 23:16:47 +0200

    Time ticker structure updated;

--- app/src/main/java/com/example/samsungclockclone/data/ticker/TimeTickerImpl.kt
+++ app/src/main/java/com/example/samsungclockclone/data/ticker/TimeTickerImpl.kt
@@ -1,6 +1,33 @@
 package com.example.samsungclockclone.data.ticker
 
 import com.example.samsungclockclone.domain.ticker.TimeTicker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class TimeTickerImpl @Inject constructor() : TimeTicker()
+class TimeTickerImpl @Inject constructor() : TimeTicker {
+
+    private val job = Job()
+    private val dispatcher = Dispatchers.Default
+    private val coroutineScope = CoroutineScope(job + dispatcher)
+    private val tickPass: MutableSharedFlow<Long> = MutableSharedFlow()
+
+    override fun onSendTick(
+        milliseconds: Long,
+    ) {
+        coroutineScope.launch {
+            tickPass.emit(milliseconds)
+        }
+    }
+
+    override fun onGetTick(): Flow<Long> = tickPass.asSharedFlow()
+
+    override fun onDestroy() {
+        job.cancel()
+    }
+}
--- app/src/main/java/com/example/samsungclockclone/domain/ticker/TimeTicker.kt
+++ app/src/main/java/com/example/samsungclockclone/domain/ticker/TimeTicker.kt
@@ -1,31 +1,12 @@
 package com.example.samsungclockclone.domain.ticker
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.launch
 
-abstract class TimeTicker {
-
-    private val job = Job()
-    private val dispatcher = Dispatchers.Default
-    private val coroutineScope = CoroutineScope(job + dispatcher)
-    private val tickPass: MutableSharedFlow<Long> = MutableSharedFlow()
-
+interface TimeTicker {
     fun onSendTick(
         milliseconds: Long,
-    ) {
-        coroutineScope.launch {
-            tickPass.emit(milliseconds)
-        }
-    }
+    )
 
-    fun onGetTick(): Flow<Long> = tickPass.asSharedFlow()
-
-    fun onDestroy() {
-        job.cancel()
-    }
+    fun onGetTick(): Flow<Long>
+    fun onDestroy()
 }
