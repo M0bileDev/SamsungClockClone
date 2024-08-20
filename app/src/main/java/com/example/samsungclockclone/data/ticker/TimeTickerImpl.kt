62d4f749ca4e3bc0086afa97072f647cd25bcc0b
m0bileDev <damian.ogorek.1996@gmail.com>
2024-08-20 23:08:28 +0200

    Basic implementation of time ticker class created;

---
+++ app/src/main/java/com/example/samsungclockclone/data/ticker/TimeTickerImpl.kt
@@ -0,0 +1,6 @@
+package com.example.samsungclockclone.data.ticker
+
+import com.example.samsungclockclone.domain.ticker.TimeTicker
+import javax.inject.Inject
+
+class TimeTickerImpl @Inject constructor() : TimeTicker()
