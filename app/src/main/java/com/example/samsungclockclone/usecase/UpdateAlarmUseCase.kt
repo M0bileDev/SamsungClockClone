b10cd4752984ddecd46cfa9d833ea28eec49aaa9
m0bileDev <damian.ogorek.1996@gmail.com>
2024-09-05 21:08:15 +0200

    Arguments added; Logic updated;

--- app/src/main/java/com/example/samsungclockclone/usecase/UpdateAlarmUseCase.kt
+++ app/src/main/java/com/example/samsungclockclone/usecase/UpdateAlarmUseCase.kt
@@ -1,11 +1,14 @@
 package com.example.samsungclockclone.usecase
 
+import android.app.AlarmManager
 import com.example.samsungclockclone.data.local.dao.AlarmDao
 import com.example.samsungclockclone.data.local.model.AlarmManagerEntity
+import com.example.samsungclockclone.domain.scheduler.AlarmScheduler
 import com.example.samsungclockclone.domain.utils.AlarmId
 import com.example.samsungclockclone.domain.utils.AlarmMode
 import com.example.samsungclockclone.domain.utils.DayOfWeek
 import com.example.samsungclockclone.domain.utils.toAlarmRepeat
+import com.example.samsungclockclone.ext.suspendCheckPermission
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -15,7 +18,9 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class UpdateAlarmUseCase @Inject constructor(
-    private val alarmDao: AlarmDao
+    private val alarmDao: AlarmDao,
+    private val alarmScheduler: AlarmScheduler,
+    private val alarmManager: AlarmManager
 ) {
 
     suspend operator fun invoke(
@@ -23,52 +28,63 @@ class UpdateAlarmUseCase @Inject constructor(
         alarmMode: AlarmMode,
         alarmMillisecondsList: List<Long>,
         selectedDaysOfWeek: List<DayOfWeek>,
+        onScheduleCompleted: () -> Unit = {},
+        onScheduleDenied: () -> Unit,
         parentScope: CoroutineScope,
         dispatcher: CoroutineDispatcher = Dispatchers.Default
     ): Job {
         return parentScope.launch(dispatcher) {
             if (!isActive) return@launch
 
-            alarmDao.updateAlarmMode(alarmId, alarmMode)
+            alarmManager.suspendCheckPermission(
+                this,
+                onPermissionGranted = {
 
-            alarmDao.deleteAlarmManagerById(alarmId)
-            // TODO: cancel alarm manager after full db implementation
+                    alarmDao.updateAlarmMode(alarmId, alarmMode)
 
-            val alarmRepeat = alarmMode.toAlarmRepeat()
-            val entities: List<AlarmManagerEntity> =
-                if (selectedDaysOfWeek.isNotEmpty()) {
-
-                    val zipped = alarmMillisecondsList.zip(selectedDaysOfWeek)
-                    zipped.map {
-                        val (milliseconds, day) = it
-                        AlarmManagerEntity(
-                            parentId = alarmId,
-                            fireTime = milliseconds,
-                            repeat = alarmRepeat,
-                            dayOfWeek = day
-                        )
-
+                    val alarmManagers = alarmDao.getAlarmManagersById(alarmId)
+                    alarmManagers.forEach { alarmManager ->
+                        alarmScheduler.cancel(alarmManager.uniqueId)
                     }
 
-                } else {
-                    alarmMillisecondsList.map { milliseconds ->
-                        AlarmManagerEntity(
-                            parentId = alarmId,
-                            fireTime = milliseconds,
-                            repeat = alarmRepeat
-                        )
-                    }
-                }
+                    alarmDao.deleteAlarmManagerById(alarmId)
 
-            alarmDao.insertAlarmMangers(entities)
+                    val alarmRepeat = alarmMode.toAlarmRepeat()
+                    val entities: List<AlarmManagerEntity> =
+                        if (selectedDaysOfWeek.isNotEmpty()) {
 
-            //TODO schedule alarms via alarm manager
-//            alarmScheduler.schedule(
-//                alarms,
-//                onScheduleCompleted = ::onScheduleCompleted,
-//                onScheduleDenied = ::onScheduleDenied
-//            )
+                            val zipped = alarmMillisecondsList.zip(selectedDaysOfWeek)
+                            zipped.map {
+                                val (milliseconds, day) = it
+                                AlarmManagerEntity(
+                                    parentId = alarmId,
+                                    fireTime = milliseconds,
+                                    repeat = alarmRepeat,
+                                    dayOfWeek = day
+                                )
 
+                            }
+
+                        } else {
+                            alarmMillisecondsList.map { milliseconds ->
+                                AlarmManagerEntity(
+                                    parentId = alarmId,
+                                    fireTime = milliseconds,
+                                    repeat = alarmRepeat
+                                )
+                            }
+                        }
+
+                    val idMillisecondsPairs = alarmDao.insertAlarmMangers(entities)
+                    alarmScheduler.schedule(
+                        idMillisecondsPairs,
+                        onScheduleCompleted = onScheduleCompleted,
+                        onScheduleDenied = onScheduleDenied
+                    )
+
+                },
+                onPermissionDenied = onScheduleDenied
+            )
         }
     }
 
