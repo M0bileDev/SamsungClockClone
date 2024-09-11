4f93eb97f2517e0d47f678355f281f6baab1e127
m0bileDev <damian.ogorek.1996@gmail.com>
2024-09-11 23:05:10 +0200

    Module binder updated;

--- app/src/main/java/com/example/samsungclockclone/di/ApplicationModuleBinder.kt
+++ app/src/main/java/com/example/samsungclockclone/di/ApplicationModuleBinder.kt
@@ -1,9 +1,13 @@
 package com.example.samsungclockclone.di
 
+import com.example.samsungclockclone.data.local.permissions.PermissionsListenerImpl
 import com.example.samsungclockclone.data.local.preferences.AlarmPreferencesImpl
+import com.example.samsungclockclone.data.local.preferences.SelectionPreferencesImpl
 import com.example.samsungclockclone.data.scheduler.AlarmSchedulerImpl
 import com.example.samsungclockclone.data.ticker.TimeTickerImpl
+import com.example.samsungclockclone.domain.permissions.PermissionsListener
 import com.example.samsungclockclone.domain.preferences.AlarmPreferences
+import com.example.samsungclockclone.domain.preferences.SelectionPreferences
 import com.example.samsungclockclone.domain.scheduler.AlarmScheduler
 import com.example.samsungclockclone.domain.ticker.TimeTicker
 import dagger.Binds
@@ -23,4 +27,10 @@ abstract class ApplicationModuleBinder {
 
     @[Binds Singleton]
     abstract fun bindTimeTicker(timeTicker: TimeTickerImpl): TimeTicker
+
+    @[Binds Singleton]
+    abstract fun bindSelectionPreferences(selectionPreferences: SelectionPreferencesImpl): SelectionPreferences
+
+    @[Binds Singleton]
+    abstract fun bindPermissionListener(permissionsListener: PermissionsListenerImpl): PermissionsListener
 }
