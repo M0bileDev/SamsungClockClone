b98fa885799322f4fa94ec9b4f3291cf879b451e
m0bileDev <damian.ogorek.1996@gmail.com>
2024-09-11 23:04:06 +0200

    Business logic created; Logic for user selections and permissions created;

---
+++ app/src/main/java/com/example/samsungclockclone/domain/permissions/SelectionPreferences.kt
@@ -0,0 +1,10 @@
+package com.example.samsungclockclone.domain.permissions
+
+import kotlinx.coroutines.flow.Flow
+
+interface PermissionsListener {
+
+    suspend fun requestPermissionPostNotification(request: Unit)
+    fun collectPermissionPostNotification(): Flow<Unit>
+
+}
