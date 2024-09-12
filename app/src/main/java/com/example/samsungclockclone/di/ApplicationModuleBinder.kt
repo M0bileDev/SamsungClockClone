19e0cebf5724275226c81841c9f6fc67260ddfc8
m0bileDev <damian.ogorek.1996@gmail.com>
2024-09-12 23:45:05 +0200

    Binders updated;

--- app/src/main/java/com/example/samsungclockclone/di/ApplicationModuleBinder.kt
+++ app/src/main/java/com/example/samsungclockclone/di/ApplicationModuleBinder.kt
@@ -1,10 +1,12 @@
 package com.example.samsungclockclone.di
 
+import com.example.samsungclockclone.data.local.dialog.DialogListenerImpl
 import com.example.samsungclockclone.data.local.permissions.PermissionsListenerImpl
 import com.example.samsungclockclone.data.local.preferences.AlarmPreferencesImpl
 import com.example.samsungclockclone.data.local.preferences.SelectionPreferencesImpl
 import com.example.samsungclockclone.data.scheduler.AlarmSchedulerImpl
 import com.example.samsungclockclone.data.ticker.TimeTickerImpl
+import com.example.samsungclockclone.domain.dialog.DialogListener
 import com.example.samsungclockclone.domain.permissions.PermissionsListener
 import com.example.samsungclockclone.domain.preferences.AlarmPreferences
 import com.example.samsungclockclone.domain.preferences.SelectionPreferences
@@ -32,5 +34,8 @@ abstract class ApplicationModuleBinder {
     abstract fun bindSelectionPreferences(selectionPreferences: SelectionPreferencesImpl): SelectionPreferences
 
     @[Binds Singleton]
-    abstract fun bindPermissionListener(permissionsListener: PermissionsListenerImpl): PermissionsListener
+    abstract fun bindDialogListener(dialogListener: DialogListenerImpl): DialogListener
+
+    @[Binds Singleton]
+    abstract fun bindPermissionsListener(permissionsListener: PermissionsListenerImpl): PermissionsListener
 }
