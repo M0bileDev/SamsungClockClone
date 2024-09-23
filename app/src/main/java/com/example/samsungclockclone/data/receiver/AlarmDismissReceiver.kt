92488e2aca12a57f3036c1693b874a7a897fad8d
m0bileDev <damian.ogorek.1996@gmail.com>
2024-09-23 17:15:07 +0200

    Dismiss action for alarm notification created;

---
+++ app/src/main/java/com/example/samsungclockclone/data/receiver/AlarmDismissReceiver.kt
@@ -0,0 +1,27 @@
+package com.example.samsungclockclone.data.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.example.samsungclockclone.domain.notification.NotificationBuilder
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class AlarmDismissReceiver : BroadcastReceiver() {
+
+    @Inject
+    lateinit var notificationBuilder: NotificationBuilder
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val notificationId = intent?.getLongExtra(NOTIFICATION_ID, -1) ?: -1
+        if (notificationId == -1L) return
+
+        notificationBuilder.cancelAlarmNotification(notificationId)
+    }
+
+    companion object {
+        const val NOTIFICATION_ID = "NOTIFICATION_ID"
+    }
+
+}
