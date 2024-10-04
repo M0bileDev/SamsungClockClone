344ea081c5e2f4ba1f2dd3b283d1c3d75ae42964
m0bileDev <damian.ogorek.1996@gmail.com>
2024-10-04 21:00:22 +0200

    Function argument updated;

--- app/src/main/java/com/example/samsungclockclone/usecase/scheduler/AlarmScheduler.kt
+++ app/src/main/java/com/example/samsungclockclone/usecase/scheduler/AlarmScheduler.kt
@@ -1,11 +1,13 @@
 package com.example.samsungclockclone.usecase.scheduler
 
 import com.example.samsungclockclone.domain.`typealias`.AlarmId
+import com.example.samsungclockclone.domain.`typealias`.AlarmManagerId
 import com.example.samsungclockclone.domain.`typealias`.AlarmMilliseconds
 
 interface AlarmScheduler {
     fun schedule(
-        alarmIdMillisecondsPairs: List<Pair<AlarmId, AlarmMilliseconds>>,
+        alarmId: AlarmId,
+        alarmManagerIdMillisecondsPairs: List<Pair<AlarmManagerId, AlarmMilliseconds>>,
         onScheduleCompleted: () -> Unit,
         onScheduleDenied: () -> Unit
     )
