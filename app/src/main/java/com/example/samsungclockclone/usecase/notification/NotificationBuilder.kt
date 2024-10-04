41770c99160b1f3336d1e27a0cd2aba985f5cb2d
m0bileDev <damian.ogorek.1996@gmail.com>
2024-10-04 21:04:04 +0200

    Type updated; Function arguments updated;

--- app/src/main/java/com/example/samsungclockclone/usecase/notification/NotificationBuilder.kt
+++ app/src/main/java/com/example/samsungclockclone/usecase/notification/NotificationBuilder.kt
@@ -1,12 +1,13 @@
 package com.example.samsungclockclone.usecase.notification
 
-import com.example.samsungclockclone.domain.`typealias`.NotificationId
+import com.example.samsungclockclone.domain.`typealias`.AlarmId
+import com.example.samsungclockclone.domain.`typealias`.AlarmManagerId
 
 
 interface NotificationBuilder {
     fun createAlarmNotificationChannel()
 
-    fun sendAlarmNotification(id: NotificationId, description: String = "")
+    fun sendAlarmNotification(id: AlarmManagerId, alarmId: AlarmId, description: String = "")
 
-    fun cancelAlarmNotification(id: NotificationId)
+    fun cancelAlarmNotification(id: AlarmManagerId)
 }
