d70c6c8f2871938413f1c3f3806de7abcf049d17
m0bileDev <damian.ogorek.1996@gmail.com>
2024-10-08 19:45:04 +0200

    Preview data value created;

--- app/src/main/java/com/example/samsungclockclone/presentation/activities/dismissAlarm/DismissAlarmUiState.kt
+++ app/src/main/java/com/example/samsungclockclone/presentation/activities/dismissAlarm/DismissAlarmUiState.kt
@@ -3,4 +3,11 @@ package com.example.samsungclockclone.presentation.activities.dismissAlarm
 data class DismissAlarmUiState(
     val fireTime: Long = 0L,
     val name: String = ""
-)
+) {
+    companion object {
+        val previewDismissAlarmUiState = DismissAlarmUiState(
+            1728332411,
+            "Morning alarm"
+        )
+    }
+}
