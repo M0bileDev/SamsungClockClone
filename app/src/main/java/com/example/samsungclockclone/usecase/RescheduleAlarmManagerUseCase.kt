bd11edec9cf7567f1abb7ed15e270d2c5d0d49c5
m0bileDev <damian.ogorek.1996@gmail.com>
2024-10-14 16:42:50 +0200

    Logic updated; Alarm manager and alarm scheduler added;

--- app/src/main/java/com/example/samsungclockclone/usecase/RescheduleAlarmManagerUseCase.kt
+++ app/src/main/java/com/example/samsungclockclone/usecase/RescheduleAlarmManagerUseCase.kt
@@ -1,8 +1,12 @@
 package com.example.samsungclockclone.usecase
 
+import android.app.AlarmManager
 import com.example.samsungclockclone.data.dataSource.local.DatabaseSource
 import com.example.samsungclockclone.domain.model.AlarmRepeat.Companion.createRepeatMillis
+import com.example.samsungclockclone.domain.`typealias`.AlarmId
 import com.example.samsungclockclone.domain.`typealias`.AlarmManagerId
+import com.example.samsungclockclone.framework.ext.suspendCheckPermission
+import com.example.samsungclockclone.usecase.scheduler.AlarmScheduler
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -12,25 +16,44 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class RescheduleAlarmManagerUseCase @Inject constructor(
-    private val databaseSource: DatabaseSource
+    private val databaseSource: DatabaseSource,
+    private val alarmScheduler: AlarmScheduler,
+    private val alarmManager: AlarmManager
 ) {
 
     suspend operator fun invoke(
+        alarmId: AlarmId,
         alarmManagerId: AlarmManagerId,
+        onPermissionDenied: () -> Unit ={},
+        onScheduleCompleted: () -> Unit = {},
+        onScheduleDenied: () -> Unit = {},
         parentScope: CoroutineScope,
         dispatcher: CoroutineDispatcher = Dispatchers.Default
     ): Job {
         return parentScope.launch(dispatcher) {
             if (!parentScope.isActive) return@launch
 
-            val alarmManager = databaseSource.getAlarmManagerById(alarmManagerId)
-            val fireTime = alarmManager.fireTime
-            val repeat = alarmManager.repeat
+            alarmManager.suspendCheckPermission(
+                this,
+                onPermissionGranted = {
+                    val alarmManager = databaseSource.getAlarmManagerById(alarmManagerId)
+                    val fireTime = alarmManager.fireTime
+                    val repeat = alarmManager.repeat
 
-            val repeatMillis = repeat.createRepeatMillis()
-            val updatedMillis = fireTime + repeatMillis
+                    val repeatMillis = repeat.createRepeatMillis()
+                    val updatedMillis = fireTime + repeatMillis
 
-            databaseSource.updateAlarmManagerOutOfDate(alarmManager.uniqueId, updatedMillis)
+                    databaseSource.updateAlarmManagerOutOfDate(alarmManager.uniqueId, updatedMillis)
+
+                    alarmScheduler.schedule(
+                        alarmId,
+                        listOf(alarmManager.uniqueId to updatedMillis),
+                        onScheduleCompleted = onScheduleCompleted,
+                        onScheduleDenied = onScheduleDenied
+                    )
+                },
+                onPermissionDenied = onPermissionDenied
+            )
         }
     }
 }
