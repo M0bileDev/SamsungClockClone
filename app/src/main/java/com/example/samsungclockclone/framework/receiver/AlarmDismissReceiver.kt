d6e37ac020503a77600fd8873faf41dd0fb856e3
m0bileDev <damian.ogorek.1996@gmail.com>
2024-10-14 20:10:10 +0200

    Use case added; Logic updated;

--- app/src/main/java/com/example/samsungclockclone/framework/receiver/AlarmDismissReceiver.kt
+++ app/src/main/java/com/example/samsungclockclone/framework/receiver/AlarmDismissReceiver.kt
@@ -3,14 +3,21 @@ package com.example.samsungclockclone.framework.receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.example.samsungclockclone.framework.receiver.AlarmReceiver.Companion.ALARM_ID
 import com.example.samsungclockclone.framework.receiver.AlarmReceiver.Companion.ALARM_MANAGER_ID
+import com.example.samsungclockclone.usecase.UpdateAlarmOngoingUseCase
 import com.example.samsungclockclone.usecase.notification.NotificationBuilder
 import com.example.samsungclockclone.usecase.ringtone.RingtoneController
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class AlarmDismissReceiver : BroadcastReceiver() {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Default)
 
     @Inject
     lateinit var ringtoneController: RingtoneController
@@ -18,9 +25,18 @@ class AlarmDismissReceiver : BroadcastReceiver() {
     @Inject
     lateinit var notificationBuilder: NotificationBuilder
 
+    @Inject
+    lateinit var updateAlarmOngoingUseCase: UpdateAlarmOngoingUseCase
+
     override fun onReceive(context: Context?, intent: Intent?) {
         val alarmManagerId = intent?.getLongExtra(ALARM_MANAGER_ID, -1L) ?: -1L
-        if (alarmManagerId == -1L) return
+        val alarmId = intent?.getLongExtra(ALARM_ID, -1L) ?: -1L
+
+        if (alarmId == -1L || alarmManagerId == -1L) return
+
+        coroutineScope.launch {
+            updateAlarmOngoingUseCase(alarmId, parentScope = this)
+        }
 
         ringtoneController.stop()
         notificationBuilder.cancelAlarmNotification(alarmManagerId)
