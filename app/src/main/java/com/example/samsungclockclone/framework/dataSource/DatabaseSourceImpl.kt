4a161ca32591426c64f533576792ad9904765c23
m0bileDev <damian.ogorek.1996@gmail.com>
2024-10-29 19:15:12 +0100

    function name updated;

--- app/src/main/java/com/example/samsungclockclone/framework/dataSource/DatabaseSourceImpl.kt
+++ app/src/main/java/com/example/samsungclockclone/framework/dataSource/DatabaseSourceImpl.kt
@@ -76,8 +76,8 @@ class DatabaseSourceImpl @Inject constructor(
     override suspend fun updateAlarmManagerOutOfDate(managerId: Long, newMillis: Long) =
         alarmDao.updateAlarmManagerOutOfDate(managerId, newMillis)
 
-    override suspend fun updateAlarmOngoingById(alarmId: AlarmId, ongoing: Boolean) =
-        alarmDao.updateAlarmOngoingById(alarmId, ongoing)
+    override suspend fun updateOngoingAlarmById(alarmId: AlarmId, ongoing: Boolean) =
+        alarmDao.updateOngoingAlarmById(alarmId, ongoing)
 
     override suspend fun getNotificationAlarm(
         alarmId: AlarmId,
