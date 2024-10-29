2c486daf6a7556a559425ed3701ca1ba8ed9ca27
m0bileDev <damian.ogorek.1996@gmail.com>
2024-10-29 19:36:12 +0100

    Function logic updated;

--- app/src/main/java/com/example/samsungclockclone/presentation/activities/dismissAlarm/DismissAlarmActivity.kt
+++ app/src/main/java/com/example/samsungclockclone/presentation/activities/dismissAlarm/DismissAlarmActivity.kt
@@ -91,7 +91,7 @@ class DismissAlarmActivity : ComponentActivity() {
                     DismissAlarmScreen(
                         dismissAlarmState = dismissAlarmState,
                         onDismiss = {
-                            dismissAlarmViewModel.onDismiss(getAlarmId(), getAlarmManagerId())
+                            dismissAlarmViewModel.onDismiss(getAlarmId())
                         }
                     )
                 }
