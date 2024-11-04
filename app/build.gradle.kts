1448f505a2a7fac869b4565dd58de7ebd89732f7
m0bileDev <damian.ogorek.1996@gmail.com>
2024-11-04 22:41:11 +0100

    Implementation added;

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
 
     testImplementation(libs.junit)
     testImplementation(libs.google.truth)
+    testImplementation(libs.mockk)
 
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
